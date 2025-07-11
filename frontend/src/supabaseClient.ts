f7883325ebf5c6db06989a32073044aec4ca2b12
Andrey Kostin <andrey.kostin.111@mail.ru>
2025-07-12 00:50:37 +0300

    [Frontend][Technical] Config to env

--- frontend/src/supabaseClient.ts
+++ frontend/src/supabaseClient.ts
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-console.log("VITE_SUPABASE_URL:", process.env.VITE_SUPABASE_URL);
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY as string;
